1faefae647abd7598fa0ffed99f95acbb8df95d2
Mishqat Abid <145700715+mishqatabid@users.noreply.github.com>
2024-05-15 15:21:16 +0500

    Update Fibonacci_Series.asm

--- Fibonacci_Series.asm
+++ Fibonacci_Series.asm
@@ -1,26 +1,29 @@
+; Calculating the Fibonacci series
+
 .386
 .model flat, stdcall
 .stack 4096
 ExitProcess PROTO,
 dwExitCode: DWORD
 .data
-    fibonacciArray DWORD 7 DUP(?) 
+    fibonacciArray DWORD 7 DUP(?)  ; Array to store Fibonacci numbers
     
 .code
 main PROC
-    mov eax, 1
-    mov fibonacciArray[0], eax
+    mov eax, 1                     ; Fib(1)
+    mov fibonacciArray[0], eax     ; Store in array
 
-    mov eax, 1
-    mov fibonacciArray[4], eax
+    mov eax, 1                     ; Fib(2)
+    mov fibonacciArray[4], eax     ; Store in array
 
-    mov ebx, 2
-    mov ecx, 5
-  FibonacciLoop:
-    mov eax, fibonacciArray[ebx * 4 - 8]
-    add eax, fibonacciArray[ebx * 4 - 4]
+    mov ebx, 2                     ; Start index from 2
+    mov ecx, 5                     ; Limit to 7 Fibonacci numbers
 
-    mov fibonacciArray[ebx * 4], eax
+FibonacciLoop: 
+    mov eax, fibonacciArray[ebx * 4 - 8]   ; Previous Fibonacci number (n-1)
+    add eax, fibonacciArray[ebx * 4 - 4]   ; Current Fibonacci number (n-2)
+
+    mov fibonacciArray[ebx * 4], eax       ; Store result in array
     inc ebx
     loop FibonacciLoop
     ret
