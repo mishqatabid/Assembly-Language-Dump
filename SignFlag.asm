8269c73d41ce90e075f408978a482905814061e3
Mishqat Abid <145700715+mishqatabid@users.noreply.github.com>
2024-05-15 15:59:57 +0500

    Create SignFlag.asm

---
+++ SignFlag.asm
@@ -0,0 +1,22 @@
+; Sign Flag
+
+.model flat, stdcall
+.stack 4096
+ExitProcess PROTO,
+dwExitCode:DWORD
+
+.data
+     myVariable DWORD 16, 12, 43
+     sum DWORD ?
+
+.code
+main PROC
+     mov cx, 0 
+     sub cx, 1
+     add cx, 2 
+     mov al, 0
+     sub al, 1
+     add al, 2
+ INVOKE ExitProcess, 0
+main ENDP
+END main
