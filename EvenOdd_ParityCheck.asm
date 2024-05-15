7bbeb7c1316b69e1a54ef507650aae6c02683a21
Mishqat Abid <145700715+mishqatabid@users.noreply.github.com>
2024-05-15 15:18:07 +0500

    Update EvenOdd_ParityCheck.asm

--- EvenOdd_ParityCheck.asm
+++ EvenOdd_ParityCheck.asm
@@ -1,3 +1,5 @@
+; Checking if the given byte has even or odd parity using the Parity flag.
+
 .386
 .model flat, stdcall
 .stack 4096
@@ -27,3 +29,6 @@ Ending:
     ret
 main ENDP
 END main
+
+; In EvenParity, the evenParityResult variable is set to 1 to indicate even parity was detected.
+; In OddParity, the oddParityResult variable is set to 1 to indicate odd parity was detected.
